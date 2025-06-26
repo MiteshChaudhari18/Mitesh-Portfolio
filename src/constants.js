3c3bb85f56cdb6b036b384ba911ba7667eebdff1
Mitesh Chaudhari <Krunal2012003@gmail.com>
2025-06-26 19:46:17 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,271 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import celebalLogo from './assets/company_logo/celebal.jpeg';
+import internpeLogo from './assets/company_logo/internpe.jpeg';
+import AccentureLogo from './assets/company_logo/Accenture.png';
+import datapolarisLogo from './assets/company_logo/datapolaris.png';
+
+// Education Section Logo's
+import rcLogo from './assets/education_logo/rc.png';
+import neriLogo from './assets/education_logo/neri.png';
+import schoolLogo from './assets/education_logo/school.jpg';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: celebalLogo,
+    role: "Celebal Intern",
+    company: "Celebal Technology",
+    date: "May 2025 - Present",
+    desc: "Summer Intern in the Node JS department",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React JS",
+      "TypeScript",
+      "Node JS",
+      "Tailwind CSS",
+      "MongoDb",
+      "Redux",
+      "Next Js",
+    ],
+  },
+  {
+    id: 1,
+    img: internpeLogo,
+    role: "INTERNSHIP At INTERNPE",
+    company: "INTERNPE",
+    date: "September 2024 - October 2024",
+    desc: "Completed an internship at Internpe where I worked on Java programming. During this time, I gained hands-on experience in developing Java-based applications.",
+    skills: [
+      "Java",
+      "OOP (Object-Oriented Programming)",
+      "Data Structures",
+      "Algorithms",
+      "SQL",
+      "JDBC",
+      "Git",
+      "Basic Debugging"
+    ],
+  },
+  {
+    id: 2,
+    img: AccentureLogo,
+    role: "Project Management Job Simulation",
+    company: "Accenture",
+    date: "May 2024",
+    desc: "Completed a project management job simulation where I learned to prepare proposals for optimal project management approaches. Gained insights into key attributes of successful project managers and the importance of communication in project management.",
+    skills: [
+      "Project Management",
+      "Communication",
+      "Leadership",
+      "Team Collaboration",
+      "Proposal Writing",
+      "Problem Solving"
+    ],
+  },
+
+  {
+    id: 3,
+    img: datapolarisLogo,
+    role: "Developer | Management Member",
+    company: "Data polaris",
+    date: "September 2024 - Present",
+    desc: "Contributed to multiple projects and managed technical and non technical tasks during club-led events and workshops.",
+    skills: [
+       "Problem Solving",
+       "Project Planning",
+       "Project Coordination",
+       "Event Management",
+       "Leadership",
+       "Team Collaboration",
+       "Communication",
+    
+       ],
+  },
+];
+
+export const education = [
+  
+  {
+    id: 0,
+    img: rcLogo,
+    school: "R C Patel Institute of Technology, Shirpur",
+    date: "Sept 2022 - present",
+    grade: "73.2%",
+    desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    degree: "Bachelor of Science - BSC (Computer Science)",
+  },
+  {
+    id: 1,
+    img: neriLogo,
+    school: "Janta High School , Neri",
+    date: "Apr 2020 - March 2022",
+    desc: "I completed my class 12 education from Janta High School , Neri, under the State board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    degree: "CBSE(XII) - PCM with Computer Science",
+  },
+  {
+    id: 2,
+    img: schoolLogo,
+    school: "Jain International School, jamner ",
+    date: "Apr 2010 - March 2020",
+    desc: " I completed my class 10 education from Jain International School, Jamner  under the CBSE board, where I studied Science with Computer.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "De-anonymizing Entities on Onion Sites Operating on the TOR Networ",
+    description:
+      "A study or method focused on identifying and uncovering the real-world identities behind entities operating hidden services (onion sites) within the TOR network, often using traffic analysis, metadata, or correlation techniques.",
+    image: imagesearchLogo,
+    tags: ["Python", "Tkinter", "Requests", "BeautifulSoup", "Pygame", "Threading", "Tor (via SOCKS proxy)"],
+    github: "https://github.com/MiteshChaudhari18/De-anonymizing-Entities-on-Onion-Sites-Operating-on-the-TOR-Network.git",
+    // webapp: "https://githubprofiledetective.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "crop prediction using machine learning ",
+    description:
+      "A smart crop prediction system that leverages machine learning algorithms to recommend the most suitable crop based on environmental conditions like temperature, humidity, soil type, and rainfall. Designed for farmers and agricultural planners, it helps optimize crop yield and decision-making through data-driven insights.",
+    image: csprepLogo,
+    tags: [ "HTML", "CSS", "JavaScript", "Machine Learning","React JS", "Node.js", "MongoDB", "Express"],
+    github: "https://github.com/MiteshChaudhari18/Crop_Management_System.git",
+    // webapp: "https://csprep.netlify.app/",
+  },
+  {
+     id: 2,
+    title: "Boolean Expression Solver",
+    description:
+      "A Boolean Expression Solver software application designed for digital logic design, computer science, and mathematics. It simplifies complex Boolean expressions using Boolean algebra rules evaluates expressions for various input values, and displays the simplified result for easy understanding and application.",
+    image: githubdetLogo,
+    tags: ["HTML", "CSS", "JavaScript", "React JS", "API"],
+    github: "https://github.com/MiteshChaudhari18/boolean-expression-solver.git",
+    // webapp: "https://githubprofiledetective.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "Password Strength Checker",
+    description:
+      "A Password Strength Checker is a tool that rates how secure a password is based on its complexity and length.",
+    image: npmLogo,
+    tags: ["React JS", "Node.js", "NPM", "Validation"],
+    github: "https://github.com/MiteshChaudhari18/Password_Strength_Checker.git",
+    webapp: "https://www.npmjs.com/package/cmtk-email-validator",
+  },
+  {
+    id: 4,
+    title: "QR Genrator",
+    description:
+      "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+    image: taskremLogo,
+    tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+    github: "https://github.com/MiteshChaudhari18/QR_CodeGenrator.git",
+    webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+  },
+  {
+    id: 5,
+    title: "Explore-The-World",
+    description:
+      "Created a visually appealing, interactive website that showcases various travel destinations around the world.",
+    image: webverLogo,
+    tags: ["HTML", "CSS", "JavaScript", "Framer Motion"],
+    github: "https://github.com/MiteshChaudhari18/Explore-The-World.git",
+    webapp: "https://webversedigital.com/",
+  },
+ 
+];
